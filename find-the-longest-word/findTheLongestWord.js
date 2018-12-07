acb60fab0c2dffc279072295649262fd25768bfc
Roland Manangan <roland.manangan@gmail.com>
2018-12-06 18:39:19 -1000

    Done

--- find-the-longest-word/findTheLongestWord.js
+++ find-the-longest-word/findTheLongestWord.js
@@ -1,3 +1,12 @@
 module.exports = function findTheLongestWord(sen) {
   // write your code in here
+  var longestWord = sen[0];
+  for(var i =1; i < sen.length; i++){
+    if(sen[i].length > longestWord.length){
+      longestWord = sen[i];
+    }else{
+      longestWord = longestWord;
+    }
+  }
+  return longestWord;
 }
