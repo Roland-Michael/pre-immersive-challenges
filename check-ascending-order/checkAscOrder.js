b55f5c509f398401ec60b372357f75c785da028f
Roland Manangan <roland.manangan@gmail.com>
2018-12-06 17:35:49 -1000

    First one commented out works, but not necessarily running through the whole array. Tried a second option, which looks like the solution, but not returning tru on all 4.

--- check-ascending-order/checkAscOrder.js
+++ check-ascending-order/checkAscOrder.js
@@ -1,3 +1,22 @@
 module.exports = function checkAscOrder(numArray) {
   // write your code in here
+
+//   for(var i=0; i < numArray.length; i++){
+//     if(numArray[i] < numArray[i+1]){
+//       return true;
+//     }else{
+//       return false;
+//     }
+//   }
+// }
+
+var acceptable = true;
+for(var i = 0; i < numArray.length; i++){
+  if (numArray[i] < numArray[i+1]){
+    acceptable = true;
+  }else{
+    return false;
+  }
+}
+return true;
 }
