f5295cd4e590511a32bf22d642acf654a6c9bd3f
Roland Manangan <roland.manangan@gmail.com>
2018-12-07 17:14:25 -1000

    done

--- cookie-problem/cookieProblem.js
+++ cookie-problem/cookieProblem.js
@@ -1,3 +1,15 @@
 module.exports = function cookieProblem(array) {
   // write your code in here
-}
+  var highNumber = Math.max(...array);
+  // console.log(highNumber);
+  var totalCookies = 0;
+  // console.log(typeof totalCookies);
+  for(i=0; i < array.length; i++){
+    totalCookies += (highNumber - array[i]);
+    // console.log("totalcookies", totalCookies);
+    }
+    return totalCookies;
+  }
+
+
+
